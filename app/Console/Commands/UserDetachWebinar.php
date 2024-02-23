4a60a48d69943f845d7c288a645d101ca7c1943b
Artem <artem.yablochnyi@yandex.ru>
2024-02-23 12:56:32 +0100

    added try catch to command

--- app/Console/Commands/UserDetachWebinar.php
+++ app/Console/Commands/UserDetachWebinar.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class UserDetachWebinar extends Command
 {
@@ -27,25 +29,29 @@ class UserDetachWebinar extends Command
      */
     public function handle()
     {
+        try {
+            $today = Carbon::today();
 
-        $today = Carbon::today();
+            // Получение всех пользователей, которые привязаны к группам с потенциально истекшей датой
+            $users = User::whereHas('groups', function ($query) use ($today) {
+                $query->where('closed_webinar_date', '<=', $today);
+            })->get();
 
-        // Получение всех пользователей, которые привязаны к группам с потенциально истекшей датой
-        $users = User::whereHas('groups', function ($query) use ($today) {
-            $query->where('closed_webinar_date', '<=', $today);
-        })->get();
+            foreach ($users as $user) {
+                // Для каждого пользователя получаем группы
+                $groups = $user->groups()->get();
 
-        foreach ($users as $user) {
-            // Для каждого пользователя получаем группы
-            $groups = $user->groups()->get();
-
-            foreach ($groups as $group) {
-                // Проверяем, что дата закрытия меньше текущей дате
-                if (Carbon::parse($group->pivot->closed_webinar_date)->endOfDay() < $today) {
-                    // Отвязываем пользователя от группы
-                    $user->groups()->detach($group->id);
+                foreach ($groups as $group) {
+                    // Проверяем, что дата закрытия меньше текущей дате
+                    if (Carbon::parse($group->pivot->closed_webinar_date)->endOfDay() < $today) {
+                        // Отвязываем пользователя от группы
+                        $user->groups()->detach($group->id);
+                    }
                 }
             }
+        } catch (Exception $e) {
+            // Логирование ошибки
+            Log::error('Error detaching users from groups: ' . $e->getMessage());
         }
     }
 }
