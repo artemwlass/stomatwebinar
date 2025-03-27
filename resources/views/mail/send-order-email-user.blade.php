25e6fc70631345a4d26822a889ff9752300c4c9e
yablochnyi <artem.yablochnyi@yandex.ru>
2025-03-27 19:05:51 +0100

    updated

--- resources/views/mail/send-order-email-user.blade.php
+++ resources/views/mail/send-order-email-user.blade.php
@@ -1,5 +1,6 @@
 Шановний(а) {{$user->name}},<br><br>
-
+Для отримання балів потрібно пройти тестування <strong>27 квітня</strong>, детальна інформація в <a href="https://t.me/+3PUxcCqrBiQyOThi" target="_blank" rel="noopener noreferrer">телеграм каналі</a>,
+<a href="https://docs.google.com/forms/d/e/1FAIpQLSfpMn732gqOYB1OxWlwZojCU_SYEJ59slA150ns4TgD7Ll_cQ/viewform?usp=preview" target="_blank" rel="noopener noreferrer">посилання на тестування</a>.<br>
 Дякуємо за замовлення на сайті stomatwebinar.com! Ваше замовлення було успішно оформлено. Важливо: запис вебінару доступний 30 днів з моменту оплати.<br><br>
 
 Ім'я: {{$user->name}}<br>
