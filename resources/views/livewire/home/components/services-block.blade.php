088f923866e6de6ac0bb640676a59034d4ab8f15
yablochnyi <artem.yablochnyi@yandex.ru>
2025-04-21 13:28:55 +0200

    вернул заголовки и добавил стили на заголовок h3Э

--- resources/views/livewire/home/components/services-block.blade.php
+++ resources/views/livewire/home/components/services-block.blade.php
@@ -17,9 +17,9 @@
                             <img class="img-fluid" src="{{asset('storage/' . $webinar->image)}}" alt="">
                             <a href="{{route('webinar.show', $webinar->slug)}}" class="card__info">
                                 <div class="card__title">
-{{--                                    <h5>--}}
-{{--                                        {{$webinar->title}}--}}
-{{--                                    </h5>--}}
+                                    <h5>
+                                        {{$webinar->title}}
+                                    </h5>
                                 </div>
                                 <div class="card__footer">
                                     <div class="card__arrow">
